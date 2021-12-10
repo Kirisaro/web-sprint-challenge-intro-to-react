76de72f5bc21051e1475f7b14126d4ad46755b27
Carlos Solano <solanoandy@gmail.com>
2021-12-10 09:32:22 -0600

    solved mvp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import 'bootstrap/dist/css/bootstrap.css';
+import Accordion from 'react-bootstrap/Accordion'
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then((resp) =>{
+      setCharacters(resp.data)
+      console.log(resp, 'big')
+    })
+  }, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+    <h1 className="Header">Characters</h1>
+    
+<Accordion>
+{characters.map((char, idx) =>{
+     return <Character char={char} eKey={idx}/>
+    })}
+</Accordion>
+  </div>
+);
 }
 
 export default App;
